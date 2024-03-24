b8eef3a085cbb8a1fcf6ca8414b52da612353a0f
skbwork <107772386+skbwork@users.noreply.github.com>
2024-03-24 06:40:31 +0530

    Minor changes

--- employee-tax-calc/src/main/java/com/santosh/imagin/exception/DuplicateEmployeeIdException.java
+++ employee-tax-calc/src/main/java/com/santosh/imagin/exception/DuplicateEmployeeIdException.java
@@ -1,6 +1,11 @@
 package com.santosh.imagin.exception;
 
 public class DuplicateEmployeeIdException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public DuplicateEmployeeIdException() {
 		super("Duplicate Employee ID");
 	}
